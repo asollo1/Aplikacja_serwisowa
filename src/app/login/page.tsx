a7d3f901c0d2c7d223118ce7c6a11b1257176d73
Hubert Kopania <niutka.gekon@gmail.com>
2024-09-27 19:20:15 +0200

    Finished login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,30 @@
+import '../globals.css'
 export default function Login_menu(){
     return(
-        <div>Login menu</div>
+        <div className="w-screen h-screen flex items-center justify-center">
+            <div className="bg-black border border-white p-10 ">
+                <h1 className="text-5xl font-extrabold mb-10">Zaloguj się</h1>
+                <form className="text-2xl">
+                    <div className="mb-5">
+                        <label className="block font-bold mb-2" itemType="username">
+                            Nazwa użytkownika
+                        </label>
+                        <input className="border border-white bg-black p-3" id="username" type="text" name="username" required/>
+                    </div>
+                    <div>
+                        <label className="block font-bold mb-2" itemType="password">
+                            Hasło
+                        </label>
+                        <input className="border border-white bg-black p-3 " id="password" type="password" name="password" required/>
+                    </div>
+                    <div className="flex items-center justify-between text-base mb-3">
+                        <a href="#" className="text-blue-500">Zapomniałeś hasła?</a>
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <input className="bg-black p-3 border border-white w-full button" type="submit" value={'Zaloguj się'}/>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
