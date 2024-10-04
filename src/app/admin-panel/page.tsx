469312e03ff985ce913d9cd2a3e74088424af453
Hubert Kopania <niutka.gekon@gmail.com>
2024-10-04 10:21:55 +0200

    Finished admin panel layout

--- src/app/admin-panel/page.tsx
+++ src/app/admin-panel/page.tsx
@@ -1,16 +1,33 @@
+"use client"
 import Title from "../componets/form/title"
 import Button from "../componets/button"
+import Panel from "./panel"
+import React, { useState } from 'react';
+const panelModes = {
+    REQUESTS: 1,
+    USERS: 2,
+    LOGS: 3,
+    DEFAULT: 4,
+};
 export default function Admin_panel(){
+    const [panelMode, setPanelMode] = useState(panelModes.DEFAULT);
+  
+    const handleButtonClick = (mode: any) => {
+      setPanelMode(mode);
+    };
     return (
         <div className="flex flex-row h-screen w-screen">
             <div className="h-full w-1/5 border-r-2 border-white p-10 flex flex-col items-center justify-center">
                 <Title label="Admin panel"></Title>
                 <div className="mt-10 text-center">
-                    <Button content="Requests"></Button>
-                    <Button content="Users"></Button>
-                    <Button content="Logs"></Button>
-                    <Button content="Log off"></Button>
+                    <Button content="Requests" onClick={() => handleButtonClick(panelModes.REQUESTS)} />
+                    <Button content="Users" onClick={() => handleButtonClick(panelModes.USERS)} />
+                    <Button content="Logs" onClick={() => handleButtonClick(panelModes.LOGS)} />
+                    <Button content="Log off" />
                 </div>
+            </div>
+            <div className="h-full w-4/5 p-10 flex flex-col">
+                <Panel options={panelMode}></Panel>
             </div>
         </div>
     )
---
+++ src/app/admin-panel/panel.tsx
@@ -0,0 +1,31 @@
+import Requests from "./requests"
+import Users from "./users"
+import Logs from "./logs"
+export default function Panel(props: {options: any}){
+        if (props.options == 1){
+            return (
+                <Requests />
+            )
+        } else if (props.options == 2) {
+            return (
+                <Users />
+            )
+
+        } else if (props.options == 3) {
+            return (
+                <Logs />
+            )
+        } else if (props.options == 4) {
+            return (
+                <div className="w-full h-full flex justify-center items-center">
+                    <p className="text-center text-2xl text-white">Witaj w panelu administratora</p>
+                </div>
+            )
+        } else {
+            return (
+                <div className="bg-red-500 text-black absolute m-auto p-10 border border-white">
+                    Error: No panel module of ID: {props.options}
+                </div>
+            )
+        }
+}
